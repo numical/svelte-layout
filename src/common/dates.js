1155ffe06cefc7c11e9cdd9a0bb1e98005159ed7
Mike Evans <mike.evans at numical.com>
2020-04-18 15:30:23 +0100

    display date values in productdetails

--- src/common/dates.js
+++ src/common/dates.js
@@ -9,3 +9,10 @@ export const fromIntervalToText = interval => {
     const date = fromIntervalToDate(interval);
     return `${months[date.getMonth()]} ${date.getFullYear()}`;
 }
+
+const calcYear = date => date.getFullYear() - 2020;
+const calcMonth = date => date.getMonth()
+
+export const fromDateToInterval = date => calcYear(date) * 12 + calcMonth(date);
+
+export const fromTodayToInterval = () => fromDateToInterval(new Date());
--- src/components/product/ProductDetails.svelte
+++ src/components/product/ProductDetails.svelte
@@ -2,6 +2,8 @@
     import { layout } from '../../stores/layoutManager';
     import { products } from '../../stores/productPresenter';
     import { format } from '../../common/currency';
+    import { fromTodayToInterval, fromIntervalToText } from '../../common/dates';
+    import { fromSVGCoordsToInterval } from '../../common/coords';
 
     export let product;
     $: style = product === $products.highlighted
@@ -11,15 +13,22 @@
                 border-radius:${$layout.overlayPanel ? '0.25rem' : '0'};
               `
             : '';
+    $: interval = $layout.dateLineX ? fromSVGCoordsToInterval({ x: $layout.dateLineX }) : fromTodayToInterval();
+    $: label = $layout.dateLineX ? `Value at ${fromIntervalToText(interval)}:` : "Today's value:";
 </script>
 
 <div class="container" style={style}>
     <div class="title">{product.name}</div>
-    <div>
-        {`Start amount: ${format(product.data[0])}`}
-    </div>
-    <div>
-        {`End amount: ${format(product.data[product.data.length - 1])}`}
+    <div class="values">
+        <div>Start value:</div>
+        <div>{format(product.data[0])}</div>
+        <div />
+        <div>{label}</div>
+        <div>{format(product.data[interval])}</div>
+        <div />
+        <div>End value:</div>
+        <div>{format(product.data[product.data.length - 1])}</div>
+        <div />
     </div>
 </div>
 
@@ -30,9 +39,16 @@
         color: white;
     }
     .title {
-        font-size: x-large;
         font-weight: bold;
         padding-bottom: 0.25rem;
+    }
+    .values {
+        display: grid;
+        grid-template-columns: auto auto 1fr;
+        grid-column-gap: 0.5rem;
+    }
+    .values div {
+       text-align: end;
     }
 </style>
 
