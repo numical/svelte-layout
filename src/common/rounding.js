5b462f4f965c7dd07759e634035e4d2f692fcf99
Mike Evans <mike.evans at numical.com>
2020-05-07 20:20:52 +0100

    better rounding for y-axis values

--- src/common/rounding.js
+++ src/common/rounding.js
@@ -1,20 +1,13 @@
-const round = (ratioRoundingFn, value) => {
+const round = (roundFn, value) => {
   const magnitude = Math.pow(10, Math.floor(Math.log10(value)));
-  if (magnitude === value) {
-    return value;
-  } else {
-    const ratio = value / magnitude;
-    if (ratio === 1) {
-      return value;
-    } else if (ratio < 2) {
-      return magnitude + ratioRoundingFn((ratio - 1) * magnitude) ;
-    } else {
-      return magnitude * ratioRoundingFn(ratio);
-    }
-  }
-}
+  const ratio = Math.floor(value / magnitude);
+  return value === magnitude
+    ? value
+    : value % magnitude === 0
+      ? value
+      : roundFn(value * 10 / magnitude) * (magnitude) / 10;
+};
 
 export const roundUpNicely = round.bind(null, Math.ceil);
 
 export const roundDownNicely = round.bind(null, Math.floor);
-
--- src/common/test/rounding.test.js
+++ src/common/test/rounding.test.js
@@ -1,63 +1,94 @@
 import { test } from 'tap';
-import { roundDownNicely, roundUpNicely } from '../rounding';
+import { roundUpNicely, roundDownNicely } from '../rounding';
 
-test('roundUpNicely - multiples of 10', t => {
+test('roundUpNicely', t => {
   [0, 10, 100, 1000, 10000, 100000, 1000000].forEach(v => {
-    t.equal(roundUpNicely(v), v);
+    t.equal(roundUpNicely(v), v, `pure powers of 10 are returned: ${v}`);
   });
-  t.end();
-});
 
-test('roundUpNicely - next magnitude', t => {
+  [50, 300, 7000, 40000, 600000, 9000000].forEach(v => {
+    t.equal(
+      roundUpNicely(v),
+      v,
+      `multiples of powers of 10 are returned: ${v}`
+    );
+  });
+
   [
-    [4, 4],
-    [8, 8],
-    [45, 50],
-    [84, 90],
-    [85, 90],
-    [900, 900],
-    [280, 300],
-    [901, 1000],
-    [3500, 4000],
-    [8500, 9000],
-    [84000, 90000],
-    [56000, 60000],
-    [120000, 120000],
-    [121000, 130000],
-    [404000, 500000],
-    [902000, 1000000],
-  ].forEach(([value, rounded]) => {
-    t.equal(roundUpNicely(value), rounded);
+    [24, 24],
+    [45, 45],
+    [55, 55],
+    [240, 240],
+    [242, 250],
+    [340, 340],
+    [839, 840],
+    [1050, 1100],
+    [4050, 4100],
+    [7050, 7100],
+    [15700, 16000],
+    [45700, 46000],
+    [55000, 55000],
+    [55324, 56000],
+    [65700, 66000],
+    [101000, 110000],
+    [251000, 260000],
+    [2090000, 2100000],
+    [4090000, 4100000],
+    [601000, 610000],
+    [9090000, 9100000],
+  ].forEach(([v, r]) => {
+    t.equal(
+      roundUpNicely(v),
+      r,
+      `rounds to next tenth of magnitude: ${v} => ${r}`
+    );
   });
+
   t.end();
 });
 
-test('roundDown - multiples of 10', t => {
+test('roundDownNicely', t => {
   [0, 10, 100, 1000, 10000, 100000, 1000000].forEach(v => {
-    t.equal(roundDownNicely(v), v);
+    t.equal(roundDownNicely(v), v, `pure powers of 10 are returned: ${v}`);
   });
-  t.end();
-});
 
-test('roundDownNicely - next magnitude', t => {
-  [
-    [4, 4],
-    [8, 8],
-    [45, 40],
-    [84, 80],
-    [85, 80],
-    [89, 80],
-    [900, 900],
-    [280, 200],
-    [901, 900],
-    [3500, 3000],
-    [8500, 8000],
-    [84000, 80000],
-    [56000, 50000],
-    [404000, 400000],
-    [902000, 900000],
-  ].forEach(([value, rounded]) => {
-    t.equal(roundDownNicely(value), rounded);
+  [50, 300, 7000, 40000, 600000, 9000000].forEach(v => {
+    t.equal(
+      roundDownNicely(v),
+      v,
+      `multiples of powers of 10 are returned: ${v}`
+    );
   });
+
+  [
+    [24, 24],
+    [45, 45],
+    [55, 55],
+    [240, 240],
+    [242, 240],
+    [340, 340],
+    [839, 830],
+    [1050, 1000],
+    [4050, 4000],
+    [7050, 7000],
+    [15700, 15000],
+    [45700, 45000],
+    [55000, 55000],
+    [55324, 55000],
+    [65700, 65000],
+    [101000, 100000],
+    [251000, 250000],
+    [2090000, 2000000],
+    [4090000, 4000000],
+    [601000, 600000],
+    [9090000, 9000000],
+  ].forEach(([v, r]) => {
+    t.equal(
+      roundDownNicely(v),
+      r,
+      `rounds to previous tenth of magnitude: ${v} => ${r}`
+    );
+  });
+
   t.end();
 });
