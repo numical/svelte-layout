08e6815efeda3c6a3bf0c735e89b7ddad25cec8d
Mike Evans <mike.evans at numical.com>
2020-05-24 09:19:13 +0100

    use localStorage for settings

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,18 +1,37 @@
 import { writable } from 'svelte/store';
 import { meta } from './meta';
+import { getSettings, saveSettings } from '../storage/localStorage';
+import { delayedAction } from '../common/delayedAction';
+
+const set = (key, value) => settings.update( state => {
+  const newState = {
+    ...state,
+    [key]: value
+  };
+  delayedAction(0, () => {
+    const { meta, set, ...toSave } = newState;
+    saveSettings({...toSave });
+  });
+  return newState;
+});
 
 const initialiseValue = (meta, [key, { defaultValue }]) => {
   meta[key] = defaultValue;
   return meta;
 }
 
-const base = {
+const initial = {
   meta,
-  set: (key, value) => settings.update( state => ({
-    ...state,
-    [key]: value
-  }))
-}
+  set,
+  ...Object.entries(meta).reduce(initialiseValue, {})
+};
 
-export const settings = writable(Object.entries(meta).reduce(initialiseValue, base));
+// in cse setting keys change, don't just blindly copy in stored values
+Object.entries(getSettings()).forEach(([key, value]) => {
+  if (initial[key] !== undefined) {
+    initial[key] = value;
+  }
+});
+
+export const settings = writable(initial);
 
---
+++ src/storage/localStorage.js
@@ -0,0 +1,16 @@
+const storageAvailable = require('storage-available')
+const enabled = storageAvailable('localStorage');
+
+const settingsKey = 'settings';
+const disabledFn = () => {};
+
+const get = key => {
+  const s = localStorage.getItem(key);
+  return s ? JSON.parse(s) : {}; 
+}
+
+const save = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+
+export const getSettings = enabled ? get.bind(null, settingsKey) : disabledFn;
+
+export const saveSettings = enabled ? save.bind(null, settingsKey) : disabledFn;
