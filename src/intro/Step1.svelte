1ba3179bbb46c3a7373ff285caac55cffe5edeca
Mike Evans <mike.evans at numical.com>
2020-05-25 10:59:59 +0100

    first setp of intro

--- src/intro/Step1.svelte
+++ src/intro/Step1.svelte
@@ -1,15 +1,32 @@
 <script>
   export let next;
+
+  const svgProps = {
+    viewBox: "0 0 1000 1000",
+    preserveAspectRatio: "none"
+  }
+
+  const borderProps = {
+    points: "10,10 10,990, 990,990 990,10 10,10",
+    stroke: "black",
+    strokeWidth: 2,
+    fill: "none"
+  }
 </script>
 
-<div>
-  <h1>Intro Page</h1>
-  <h2>Step 1</h2>
-  <a href="#" on:click="{next}">>></a>
-</div>
+<svg {...svgProps}>
+
+  <polyline {...borderProps} />
+  <line x1="100" x2="900" y1="100" y2="100" />
+  <line x1="100" x2="900" y1="500" y2="500" />
+  <line x1="100" x2="900" y1="900" y2="900" />
+</svg>
 
 <style>
-  div {
-    text-align: center;
+  svg {
+    background-color: lightblue;
+  }
+  line {
+    stroke: darkorange;
   }
 </style>
